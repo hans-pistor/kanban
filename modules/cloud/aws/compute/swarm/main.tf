0e9c4ef7d554987eefd60db2d3a5f053b6fcf367
Hans Pistor <hpistor@fastmail.com>
2024-06-23 05:31:57 -0700

    Setup terraform to start ec2 compute

---
+++ modules/cloud/aws/compute/swarm/main.tf
@@ -0,0 +1,147 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.13.1"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.4"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "2.4.0"
+    }
+  }
+}
+
+data "aws_vpc" "main" {
+  filter {
+    name   = "isDefault"
+    values = ["true"]
+  }
+}
+
+data "aws_subnets" "main_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.main.id]
+  }
+}
+
+data "aws_ami" "amazon_linux_docker" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amazon-linux-docker*"]
+  }
+
+  owners = ["647664114478"]
+}
+
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = "4096"
+}
+
+resource "local_sensitive_file" "private_key" {
+  filename        = var.private_key_path
+  content         = tls_private_key.rsa.private_key_openssh
+  file_permission = "0400"
+}
+
+resource "aws_key_pair" "deployer_key" {
+  key_name   = "swarm-key"
+  public_key = tls_private_key.rsa.public_key_openssh
+}
+
+resource "aws_instance" "my_swarm" {
+  ami               = data.aws_ami.amazon_linux_docker.id
+  availability_zone = "us-west-2b"
+  instance_type     = "t2.micro"
+  key_name          = aws_key_pair.deployer_key.key_name
+  subnet_id         = data.aws_subnets.main_subnets.ids[2]
+  tags = {
+    "Name" = "docker-swarm-manager"
+  }
+  vpc_security_group_ids = [
+    aws_security_group.swarm_sg.id
+  ]
+  user_data = <<-EOF
+  #!/bin/bash
+  sudo dnf update -y \
+  && sudo dnf install -y docker \
+  && sudo systemctl start docker \
+  && sudo systemctl enable docker \
+  && sudo usermod -a -G docker ec2-user \
+  && newgrp docker
+  EOF
+}
+
+
+
+resource "aws_security_group" "swarm_sg" {
+  description = "Swarm SG"
+  vpc_id      = data.aws_vpc.main.id
+  egress = [
+    {
+      cidr_blocks = [
+        "0.0.0.0/0",
+      ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    },
+  ]
+  ingress = [
+    {
+      cidr_blocks = [
+        "0.0.0.0/0",
+      ]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    },
+    {
+      cidr_blocks = [
+        "0.0.0.0/0",
+      ]
+      description      = ""
+      from_port        = 443
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 443
+    },
+    {
+      cidr_blocks = [
+        "0.0.0.0/0",
+      ]
+      description      = ""
+      from_port        = 4000
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 4000
+    },
+  ]
+  name                   = "swarm-pool-ports"
+  revoke_rules_on_delete = false
+}
