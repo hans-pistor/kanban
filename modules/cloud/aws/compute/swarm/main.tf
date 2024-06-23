7ef5418c2c159ef33ab71ca8b3ff4979a4f9b0a9
Hans Pistor <hpistor@fastmail.com>
2024-06-23 11:07:32 -0700

    Extract terraform config to separate file

--- modules/cloud/aws/compute/swarm/main.tf
+++ modules/cloud/aws/compute/swarm/main.tf
@@ -1,26 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.13.1"
-    }
-
-    tls = {
-      source  = "hashicorp/tls"
-      version = "4.0.4"
-    }
-
-    local = {
-      source  = "hashicorp/local"
-      version = "2.4.0"
-    }
-
-    null = {
-      source  = "hashicorp/null"
-      version = "3.2.1"
-    }
-  }
-}
 
 locals {
   init_script = file("${path.module}/scripts/initialize.sh")
